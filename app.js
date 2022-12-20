089f7ba82475f57ffdc3107a2585b976f50d8f69
Naresh C <naresh_dev@Nareshs-MacBook-Pro.local>
2022-12-20 23:12:24 +0530

    update auth setup

--- app.js
+++ app.js
@@ -74,6 +74,9 @@ const config = {
     auth0Logout: true,
     baseURL: process.env.BASE_URL,
     clientID: process.env.CLIENT_ID,
+    cookie: {
+        domain: process.env.SERVER_DOMAIN
+    },
     issuerBaseURL: process.env.ISSUER_BASE_URL,
     secret: process.env.SECRET,
     routes: {
