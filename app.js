fefe8acc5db6a8cbe90802e5cdb2f4a87a347be2
Naresh C <naresh_dev@Nareshs-MacBook-Pro.local>
2022-12-23 03:21:42 +0530

    update auth0 callback url 2

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ const config = {
     issuerBaseURL: process.env.ISSUER_BASE_URL,
     secret: process.env.SECRET,
     routes: {
-        callback : '/app/callback',
+        callback : '/callback',
         login: false,
         postLogoutRedirect: '/api/logout'
     }
