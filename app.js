8177fa7b01fab1afa87827de6d9acc3a5de71ba1
Naresh C <naresh_dev@Nareshs-MacBook-Pro.local>
2022-12-25 01:50:59 +0530

    add callback config

--- app.js
+++ app.js
@@ -78,6 +78,7 @@ const config = {
     issuerBaseURL: process.env.ISSUER_BASE_URL,
     secret: process.env.SECRET,
     routes: {
+        callback : '/app/callback',
         login: false,
         postLogoutRedirect: '/api/logout'
     }
