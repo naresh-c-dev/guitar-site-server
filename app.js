21e0d9bffe305b14c7e4da54e2b035fd046f9f7c
Naresh C <naresh_dev@Nareshs-MacBook-Pro.local>
2022-12-25 06:54:14 +0530

    add custom callback.

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/app/callback', (req, res, next) => {
+app.use((req, res, next) => {
     req.url = '/app' + req.url;
     next();
   });
